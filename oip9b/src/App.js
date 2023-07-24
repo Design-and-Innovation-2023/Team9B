259908842b691a2377edc955e5b99978ba2cb711
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-07-24 17:09:08 +0100

    added component to execute and display python

--- oip9b/src/App.js
+++ oip9b/src/App.js
@@ -1,8 +1,6 @@
 import CurioController                        from './components/CurioController';
 import Python                                 from './components/Python';
 import React, { useRef, useEffect, useState } from "react";
-
-
 const App = () => {
 
   /*----------------------------------------------------------------------------------------------------------------*/
@@ -16,27 +14,31 @@ const App = () => {
     }
   }, [runOnce]);
   /*----------------------------------------------------------------------------------------------------------------*/
-  
-  return (
+  const python_codes  = 
+  `
+    import numpy             as np
+    import matplotlib.pyplot as plt
+    x = np.array([2,3,4,5,6,7,8])
+    y = np.array([3,2,1,0,1,2,3])
+    fig, ax = plt.subplots()
+    plt.scatter( x , y )
+    display(fig, target="matplotlibplot")
+    print("Hello World")
+  `;
+  /*----------------------------------------------------------------------------------------------------------------*/
+    return (
     <>
-        <React.StrictMode>
-                <center>
-                    <div>
-                        <h1>{greeting}</h1>
-                    </div>
-                </center>
+        <React.StrictMode><center><div><h1>{greeting}</h1></div></center>
             <CurioController />
-            <Python value='Hello World' />
+            <Python codes={python_codes} />
         </React.StrictMode>       
     </>
   );
   /*----------------------------------------------------------------------------------------------------------------*/
 }
-
 App.getLayout = (page) => (
   <>
       {page}
   </>
 );
-
 export default App;
--- oip9b/src/components/Python.js
+++ oip9b/src/components/Python.js
@@ -1,13 +1,28 @@
-export default function Python( {value} ) {
+import {PyScriptProvider,PyScript} from 'pyscript-react' ;
+// https://github.com/Py4Js/pyscript-react
+// https://pyscript.net/examples/matplotlib.html
+export default function Python( {codes} ) {
 
     return(
             <>
-                <div
-                    dangerouslySetInnerHTML={{
-                                                __html: `<py-script>${value}</py-script>`,
-                                            }}
-                />
+                <>
+                    <PyScriptProvider>
+                        <div id="matplotlibplot" name="matplotlibplot">Output</div>
+                        {/* 
+                        <PyScript 
+                                output="matplotlibplot" 
+                                generateOutputTag 
+                                pyConfigProps={{ type: "json" , packages: new Set(["numpy","pandas","matplotlib"]), }} /> 
+                        */}
+                        <py-config>
+                            packages = ["numpy","pandas","matplotlib"]
+                            {/* plugins  = [] */}
+                        </py-config>
+                        <py-script>
+                        {codes}
+                        </py-script>
+                    </PyScriptProvider>
+                </>
             </>
           );
-
 };
