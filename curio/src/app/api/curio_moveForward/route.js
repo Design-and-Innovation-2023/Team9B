fa6ac480061fb87a59b34b170e0d42071c66a18e
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-07-26 11:19:25 +0100

    update

--- curio/src/app/api/curio_moveForward/route.js
+++ curio/src/app/api/curio_moveForward/route.js
@@ -2,17 +2,11 @@ import { global_curio , Curio } from '../../components/Curio';
 
 const curio = new Curio();
 
-export async function GET( request )
+export function GET( request )
 {
-    if( global_curio.getConnection() )
-    {
+
         global_curio.forward();
         console.log("forward()");
         return new Response('forward()')
-    }
-    else
-    {
-        console.log("Not Connected");
-        return new Response('Not Connected')
-    }
+
 }
