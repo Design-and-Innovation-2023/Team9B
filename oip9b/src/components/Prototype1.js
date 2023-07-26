b9821f6ae678097d52c89ce73c25c0adfff435f8
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-07-26 11:21:58 +0100

    remove React app

--- oip9b/src/components/Prototype1.js
+++
@@ -1,9 +0,0 @@
-
-export default function Prototype1() {
-    return(
-            <div>
-                <span>def </span><input type="text" />
-                    
-            </div>
-    );
-};
